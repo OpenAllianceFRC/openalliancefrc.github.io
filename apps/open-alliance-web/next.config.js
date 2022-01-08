cfe2a99e8715b767c152b0b8a90ea4024844d97f
Ty Tremblay <ty@rtr.ai>
2022-01-08 10:38:31 -0500

    github pages via actions?

--- apps/open-alliance-web/next.config.js
+++ apps/open-alliance-web/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const withTM = require("next-transpile-modules")(["ui"]);
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withTM({
   reactStrictMode: true,
+  assetPrefix: isProd ? '/openalliancefrc.github.io/' : ''
 })
